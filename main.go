d75efcc4cb9dfc531e5cd485ac6adf351582eb75
zhongliang <zhongliang_01@hotmail.com>
2024-09-20 15:52:51 +0800

    ready to run

--- main.go
+++ main.go
@@ -7,15 +7,14 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/Kid-debug/packer-plugin-alicloud/version"
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
 	"github.com/myklst/packer-plugin-alicloud/alicloud/datasource"
 )
-j
+
 func main() {
 	pps := plugin.NewSet()
 	pps.RegisterDatasource("datasource", new(datasource.Datasource))
-	pps.SetVersion(version.PluginVersion)
+	// pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
